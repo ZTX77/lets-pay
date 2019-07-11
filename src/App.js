55e8a67f6ae9c9a0c8c0f2293aae57fcb386c312
Zoli <zoly.toth87@gmail.com>
2019-07-11 19:10:25 +0300

    added export functionalities & css fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import React, {useState, useEffect, useReducer} from 'react';
 import './App.css';
 import RowComponent from './components/RowComponent';
+import html2canvas from 'html2canvas';
+
+const genIMG = () => {
+    // html2canvas(document.getElementById("root")).then( canvas =>{
+    //     var img = canvas.toDataURL('image/png', 100);
+    //     var doc = new jsPDF();
+    //     doc.addImage(img, 'png', 20, 20);
+    //     doc.save('test.pdf')
+    // });
+    // html2canvas(document.getElementById("root")).then(function (canvas) {
+    //     canvas.toBlob(function (blob) {
+    //         window.saveAs(blob, "img.png");
+    //     });
+    // });
+    html2canvas(document.getElementById("root")).then( canvas =>{
+        var img = canvas.toDataURL('image/jpeg', 1);
+        var newTab= window.open();
+        newTab.document.write('<img src="' + img + '" alt="img" />');
+    });
+};
 
 function appReducer(state, action) {
   switch (action.type) {
@@ -42,6 +62,11 @@ const App = () => {
 
   const finalValue = (value, specialValue) => {
       return ((value - (specialValue * -1)) / 3).toFixed(2);
+  };
+
+  const handleClick = () => {
+      genIMG();
+      setTimeout(() => window.open("https://login.yahoo.com/"), 2000);
   };
 
   // const handleChange = e => {
@@ -109,7 +134,8 @@ const App = () => {
             />
         </div>
             <div className='totalToPay'>
-                Total: {finalSum.toFixed(2)}
+                <i className="material-icons" onClick={handleClick}>get_app</i>
+                <span className='totalText'>Total: {finalSum.toFixed(2)}</span>
             </div>
     </div>
   );
