c3c0853be5a9a348b51a5249cb5229e2a6e92f4c
Zoli <zoly.toth87@gmail.com>
2019-07-11 19:13:43 +0300

    Updates

---
+++ precache-manifest.d2b1451437ba06d8181a63afcba5c4f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "278bbb7627311cc69a974b9e7f748c0a",
+    "url": "/lets-pay/index.html"
+  },
+  {
+    "revision": "2d772e1d3731fb05f1c8",
+    "url": "/lets-pay/static/css/main.1bae4dfe.chunk.css"
+  },
+  {
+    "revision": "37822a26eb91193fbb93",
+    "url": "/lets-pay/static/js/2.beb5f84e.chunk.js"
+  },
+  {
+    "revision": "2d772e1d3731fb05f1c8",
+    "url": "/lets-pay/static/js/main.02b80f3b.chunk.js"
+  },
+  {
+    "revision": "5b83a8b9876b4b661bcf",
+    "url": "/lets-pay/static/js/runtime~main.311cf2ca.js"
+  },
+  {
+    "revision": "785719ac83ed5b3d9312ef4f35563995",
+    "url": "/lets-pay/static/media/blue-nebula.785719ac.jpg"
+  }
+]);
